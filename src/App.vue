cb6e885ce6c8b17ebfa24083b092343d0dfd52af
wanglei <wanglei582@vip.qq.com>
2020-07-09 21:07:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less"></style>
